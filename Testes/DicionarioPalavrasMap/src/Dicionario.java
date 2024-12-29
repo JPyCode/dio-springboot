e8e3d5399702c6625abac968e6f6374cfb9f5183
JPyCode <darkeverwing@gmail.com>
2024-12-28 23:51:43 -0300

    Adicionado os arquivos teste do projeto Dicionario
    Signed-off-by: JPyCode <darkeverwing@gmail.com>

---
+++ Testes/DicionarioPalavrasMap/src/Dicionario.java
@@ -0,0 +1,53 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Dicionario {
+
+    private Map<String, String> dicionario;
+
+    public Dicionario() {
+        this.dicionario = new HashMap<>();
+    }
+
+    void adicionarPalavra(String palavra, String definicao) {
+        dicionario.put(palavra, definicao);
+    }
+
+    void removerPalavra(String palavra) {
+        dicionario.remove(palavra);
+    }
+
+    void exibirPalavras() {
+        dicionario.forEach((palavra, definicao) -> {
+            System.out.println(palavra + " : " + definicao);
+        });
+    }
+
+    void pesquisarPorPalavra(String palavra) {
+        System.out.println(dicionario.get(palavra));
+    }
+
+
+    public static void main(String[] args) {
+        Dicionario dicionario = new Dicionario();
+    
+        // Adicionar palavras (linguagens de programação)
+        dicionario.adicionarPalavra("java", "Linguagem de programação orientada a objetos.");
+        dicionario.adicionarPalavra("typescript", "Superset da linguagem JavaScript que adiciona tipagem estática.");
+        dicionario.adicionarPalavra("kotlin", "Linguagem moderna de programação para a JVM.");
+    
+        // Exibir todas as palavras
+        dicionario.exibirPalavras();
+    
+        // Pesquisar palavras
+        dicionario.pesquisarPorPalavra("java");
+    
+        
+        dicionario.pesquisarPorPalavra("csharp");
+    
+        // Remover uma palavra
+        dicionario.removerPalavra("typescript");
+        dicionario.exibirPalavras();
+      }
+
+}
