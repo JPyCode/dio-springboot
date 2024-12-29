44a8096a546e02cf0c9352e27718948705a7d365
JPyCode <darkeverwing@gmail.com>
2024-12-29 00:00:33 -0300

    editing lines
    Signed-off-by: JPyCode <darkeverwing@gmail.com>

--- Testes/DicionarioPalavrasMap/src/Dicionario.java
+++ Testes/DicionarioPalavrasMap/src/Dicionario.java
@@ -31,21 +31,17 @@ public class Dicionario {
     public static void main(String[] args) {
         Dicionario dicionario = new Dicionario();
     
-        // Adicionar palavras (linguagens de programação)
         dicionario.adicionarPalavra("java", "Linguagem de programação orientada a objetos.");
         dicionario.adicionarPalavra("typescript", "Superset da linguagem JavaScript que adiciona tipagem estática.");
         dicionario.adicionarPalavra("kotlin", "Linguagem moderna de programação para a JVM.");
     
-        // Exibir todas as palavras
         dicionario.exibirPalavras();
     
-        // Pesquisar palavras
         dicionario.pesquisarPorPalavra("java");
     
         
         dicionario.pesquisarPorPalavra("csharp");
     
-        // Remover uma palavra
         dicionario.removerPalavra("typescript");
         dicionario.exibirPalavras();
       }
